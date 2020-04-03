b846020a2ec4cb0a80ee8fbcbe09a427af2ca0a3
Paul <paul.retail@me.com>
2020-04-04 00:13:25 +0200

    del and get sub

--- controllers/subscription/lib.js
+++ controllers/subscription/lib.js
@@ -92,7 +92,6 @@ async function getSubs(req, res) {
 
     const { email } = req.body;
 
-    // On check en base si l'abonnement existe déjà
     try {
         const findUser = await User.findOne({
             email
@@ -138,8 +137,98 @@ async function getSubs(req, res) {
     }
 }
 
+async function getSub(req, res) {
 
+    const { email, name } = req.body;
+
+    try {
+        const findUser = await User.findOne({
+            email
+        });
+
+        var bufferSub;
+        const periods = [];
+        var returnSub;
+        for (const sub of findUser.subscriptions) {
+            const findSub = await Subscription.findById({_id: sub});
+            if (findSub.name == name){
+                bufferSub = findSub;
+                for(const period of bufferSub.periods) {
+                    const findPeriod = await Period.findById({_id: period});
+                    periods.push(
+                        {
+                            id: findPeriod._id,
+                            start: findPeriod.start,
+                            end: findPeriod.end,
+                            frequency: findPeriod.frequency,
+                            price: findPeriod.price,
+                            type: findPeriod.type
+                        }
+                    );
+                }
+                returnSub = {
+                    id: bufferSub._id,
+                    name: bufferSub.name, 
+                    note: bufferSub.note,
+                    website_link: bufferSub.website_link,
+                    logo_path: bufferSub.logo_path,
+                    periods: periods
+                }
+            }
+            
+        }
+        
+        return res.status(200).json({
+            data: returnSub
+        });
+
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
+}
+
+
+async function delSub(req, res) {
+    const { email, name } = req.body;
+    try {
+        var findUser = await User.findOne({
+            email
+        });
+
+        var index = 0;
+        for (const sub of findUser.subscriptions) {
+            const findSub = await Subscription.findById({_id: sub});
+            if (findSub.name == name){
+                //delete period then sub
+                for(const period of findSub.periods) {
+                    Period.deleteOne({ _id: period }, function (err) {
+                        if(err) console.log(err);
+                        console.log("Successful deletion");
+                      });
+                }
+                Subscription.deleteOne({ _id: findSub._id }, function (err) {
+                    if(err) console.log(err);
+                    console.log("Successful deletion");
+                  });
+
+                findUser.subscriptions.splice(index, 1)
+
+                await findUser.save();
+            }
+            index = index + 1;
+        }
+        
+        return res.status(200).json({
+            text: "Successful deletion"
+        });
+
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
+}
 
 //On exporte nos deux fonctions
 exports.addSub = addSub;
 exports.getSubs = getSubs;
+exports.getSub = getSub;
+exports.delSub = delSub;
--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -2,5 +2,7 @@ const subscription = require('./subscription/lib.js');
 
 module.exports = function (app) {
     app.post('/add',subscription.addSub);
-    app.post('/get',subscription.getSubs);
+    app.post('/getAll',subscription.getSubs);
+    app.post('/get',subscription.getSub);
+    app.post('/del',subscription.delSub);
 }
